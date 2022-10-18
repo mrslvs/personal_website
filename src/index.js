c5c0c26faa5adcf6d5a3a567f638f59d2a0b53cc
mrslvs <sekerka.miroslav@outlook.com>
2022-10-19 01:06:02 +0200

    add Components & Assets folder structure

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './Assets/Styles/index.css';
+import App from './App';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="*" element={<App />}></Route>
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
+    <React.StrictMode>
+        <BrowserRouter>
+            <Routes>
+                <Route path="*" element={<App />}></Route>
+            </Routes>
+        </BrowserRouter>
+    </React.StrictMode>
 );
