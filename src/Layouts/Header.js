2989aaffa08061ea5e8ccae1eb734e5b79b57b26
mrslvs <sekerka.miroslav@outlook.com>
2022-10-19 01:32:12 +0200

    add smooth scrolling using Link from react-scroll

--- src/Layouts/Header.js
+++ src/Layouts/Header.js
@@ -8,7 +8,18 @@ const Header = () => {
             <nav>
                 <ul>
                     <li>
-                        <Link></Link>
+                        <Link to="about" spy={true} smooth={true} duration={500}>
+                            About
+                        </Link>
+                        <Link to="projects" spy={true} smooth={true} duration={500}>
+                            Projects
+                        </Link>
+                        <Link to="skills" spy={true} smooth={true} duration={500}>
+                            Skills
+                        </Link>
+                        <Link to="contact" spy={true} smooth={true} duration={500}>
+                            Contact
+                        </Link>
                     </li>
                 </ul>
             </nav>
