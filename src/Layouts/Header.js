322e4f8452063723c8fae3259f43ed7e6985c4c9
mrslvs <sekerka.miroslav@outlook.com>
2022-10-19 01:35:27 +0200

    fix & style header

--- src/Layouts/Header.js
+++ src/Layouts/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-scroll';
+import '../Assets/Styles/header.css';
 
 const Header = () => {
     return (
@@ -11,12 +12,18 @@ const Header = () => {
                         <Link to="about" spy={true} smooth={true} duration={500}>
                             About
                         </Link>
+                    </li>
+                    <li>
                         <Link to="projects" spy={true} smooth={true} duration={500}>
                             Projects
                         </Link>
+                    </li>
+                    <li>
                         <Link to="skills" spy={true} smooth={true} duration={500}>
                             Skills
                         </Link>
+                    </li>
+                    <li>
                         <Link to="contact" spy={true} smooth={true} duration={500}>
                             Contact
                         </Link>
