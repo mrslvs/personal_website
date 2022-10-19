1cc36b65c7c4ad23cc157ac64ffcef3749faa75b
mrslvs <sekerka.miroslav@outlook.com>
2022-10-19 21:31:29 +0200

    add portrait & header styling

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import portrait from '../Assets/Images/linkedin_profile_photo.jpeg';
 
 const About = () => {
-    return <section id="about">About</section>;
+    return (
+        <section id="about">
+            {/* <img src={portrait} /> */}
+            <p>IT student based in Bratislava with passion for web development.</p>
+            <p>Currently learning ReactJS and NodeJS</p>
+        </section>
+    );
 };
 
 export default About;
