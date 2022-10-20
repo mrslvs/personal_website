5e79266cc2560f4b4d7711654c468a168b124646
mrslvs <sekerka.miroslav@outlook.com>
2022-10-20 22:00:02 +0200

    fix phone loading 70 pixels lower

--- src/Layouts/Main.js
+++ src/Layouts/Main.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import '../Assets/Styles/main.css';
 import About from '../Components/About';
 import Contact from '../Components/Contact';
@@ -6,6 +6,11 @@ import Projects from '../Components/Projects';
 import Skills from '../Components/Skills';
 
 const Main = () => {
+    useEffect(() => {
+        // fix mobile loading 70 pixels lower
+        window.scrollTo(0, 0);
+    }, []);
+
     return (
         <main>
             <About />
