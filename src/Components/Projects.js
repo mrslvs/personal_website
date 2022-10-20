f5736555966b8f09163426a77d7f63d6b5b77320
mrslvs <sekerka.miroslav@outlook.com>
2022-10-20 20:06:09 +0200

    add content & responsive styling to the projects section

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../Assets/Styles/projects.css';
 import { GoLogoGithub } from 'react-icons/go';
+import { AiFillGithub } from 'react-icons/ai';
 import todo from '../Assets/Images/todo.png';
 import fakturacia from '../Assets/Images/fakturacia.png';
 
@@ -10,19 +11,32 @@ const Projects = () => {
             <h2>My projects</h2>
             <div className="projects-container">
                 <div className="portfolio-project">
-                    <h3>Javascript TODO app</h3>
                     <img src={todo} />
+                    <h3>Javascript TODO app</h3>
                     <p>My first vanilla javascript project with emphasis on OOP </p>
-                    <GoLogoGithub className="github-link" />
+                    {/* <GoLogoGithub className="github-link" /> */}
+                    <div className="github-link-container">
+                        <a href="https://github.com/mrslvs/todo" className="github-link">
+                            <AiFillGithub className="github-logo" />
+                            <span>Github</span>
+                        </a>
+                    </div>
                 </div>
                 <div className="portfolio-project">
-                    <h3>Fullstack app</h3>
                     <img src={fakturacia} />
+                    <h3>Fullstack app</h3>
                     <p>
                         My first fullstack javascript application (still in progress) using MySQL,
-                        ExpressJS, ReactJS, NodeJS. More information available on GitHub.
+                        ExpressJS, ReactJS, NodeJS.
                     </p>
-                    <GoLogoGithub className="github-link" />
+                    <p>More information available on GitHub.</p>
+                    {/* <GoLogoGithub className="github-link" /> */}
+                    <div className="github-link-container">
+                        <a href="https://github.com/mrslvs/todo" className="github-link">
+                            <AiFillGithub className="github-logo" />
+                            <span>Github</span>
+                        </a>
+                    </div>
                 </div>
             </div>
         </section>
