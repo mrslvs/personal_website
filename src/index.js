dd616fd5c23341c1ee2622399f9dbd7eebdc2820
mrslvs <sekerka.miroslav@outlook.com>
2022-10-22 11:55:40 +0200

    add Helmet with title

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Header from './Layouts/Header';
 import Footer from './Layouts/Footer';
 import Main from './Layouts/Main';
+import { Helmet } from 'react-helmet';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -14,6 +15,9 @@ root.render(
                 <Route path="*" element={<App />}></Route>
             </Routes>
         </BrowserRouter> */}
+        <Helmet>
+            <title>Miroslav Sekerka</title>
+        </Helmet>
         <Header />
         <Main />
         {/* <Footer /> */}
